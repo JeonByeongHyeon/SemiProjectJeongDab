0aba2f73740b84e57ce92f6af663ece141ec5e74
JeonByeongHyeon <iedl97@naver.com>
2023-06-21 17:46:33 +0900

    memberVO,serviceBoardVO 추가

--- semiProject-JeongDab-Check-In-To-U/src/main/java/org/kosta/jeongdab/tou/model/MemberVO.java
+++ semiProject-JeongDab-Check-In-To-U/src/main/java/org/kosta/jeongdab/tou/model/MemberVO.java
@@ -1,5 +1,85 @@
 package org.kosta.jeongdab.tou.model;
 
-public class MemberVO {
+import java.util.Date;
 
+public class MemberVO {
+	private long memberNo;
+	private String memberEmail;
+	private String memberName;
+	private String password;
+	private Date memberBirth;
+	private int memberStatus;
+	private String memberAddress;
+	private String memberProfile;
+	public MemberVO() {
+		super();
+		
+	}
+	public MemberVO(long memberNo, String memberEmail, String memberName, String password, Date memberBirth,
+			int memberStatus, String memberAddress, String memberProfile) {
+		super();
+		this.memberNo = memberNo;
+		this.memberEmail = memberEmail;
+		this.memberName = memberName;
+		this.password = password;
+		this.memberBirth = memberBirth;
+		this.memberStatus = memberStatus;
+		this.memberAddress = memberAddress;
+		this.memberProfile = memberProfile;
+	}
+	public long getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(long memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getMemberBirth() {
+		return memberBirth;
+	}
+	public void setMemberBirth(Date memberBirth) {
+		this.memberBirth = memberBirth;
+	}
+	public int getMemberStatus() {
+		return memberStatus;
+	}
+	public void setMemberStatus(int memberStatus) {
+		this.memberStatus = memberStatus;
+	}
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+	public String getMemberProfile() {
+		return memberProfile;
+	}
+	public void setMemberProfile(String memberProfile) {
+		this.memberProfile = memberProfile;
+	}
+	@Override
+	public String toString() {
+		return "MemberVO [memberNo=" + memberNo + ", memberEmail=" + memberEmail + ", memberName=" + memberName
+				+ ", password=" + password + ", memberBirth=" + memberBirth + ", memberStatus=" + memberStatus
+				+ ", memberAddress=" + memberAddress + ", memberProfile=" + memberProfile + "]";
+	}
+	
 }
--- semiProject-JeongDab-Check-In-To-U/src/main/java/org/kosta/jeongdab/tou/model/ServiceBoardVO.java
+++ semiProject-JeongDab-Check-In-To-U/src/main/java/org/kosta/jeongdab/tou/model/ServiceBoardVO.java
@@ -1,5 +1,87 @@
 package org.kosta.jeongdab.tou.model;
 
-public class ServiceBoardVO {
+import java.util.Date;
 
+public class ServiceBoardVO {
+	private long serviceBoardNo;
+	private String serviceBoardTitle;
+	private String serviceBoardContent;
+	private Date serviceBoardCreateDate;
+	private long serviceBoardHits;
+	private String nation;
+	private String memberProfile;
+	private String serviceBoardImg;
+	public ServiceBoardVO() {
+		super();
+		
+	}
+	public ServiceBoardVO(long serviceBoardNo, String serviceBoardTitle, String serviceBoardContent,
+			Date serviceBoardCreateDate, long serviceBoardHits, String nation, String memberProfile,
+			String serviceBoardImg) {
+		super();
+		this.serviceBoardNo = serviceBoardNo;
+		this.serviceBoardTitle = serviceBoardTitle;
+		this.serviceBoardContent = serviceBoardContent;
+		this.serviceBoardCreateDate = serviceBoardCreateDate;
+		this.serviceBoardHits = serviceBoardHits;
+		this.nation = nation;
+		this.memberProfile = memberProfile;
+		this.serviceBoardImg = serviceBoardImg;
+	}
+	public long getServiceBoardNo() {
+		return serviceBoardNo;
+	}
+	public void setServiceBoardNo(long serviceBoardNo) {
+		this.serviceBoardNo = serviceBoardNo;
+	}
+	public String getServiceBoardTitle() {
+		return serviceBoardTitle;
+	}
+	public void setServiceBoardTitle(String serviceBoardTitle) {
+		this.serviceBoardTitle = serviceBoardTitle;
+	}
+	public String getServiceBoardContent() {
+		return serviceBoardContent;
+	}
+	public void setServiceBoardContent(String serviceBoardContent) {
+		this.serviceBoardContent = serviceBoardContent;
+	}
+	public Date getServiceBoardCreateDate() {
+		return serviceBoardCreateDate;
+	}
+	public void setServiceBoardCreateDate(Date serviceBoardCreateDate) {
+		this.serviceBoardCreateDate = serviceBoardCreateDate;
+	}
+	public long getServiceBoardHits() {
+		return serviceBoardHits;
+	}
+	public void setServiceBoardHits(long serviceBoardHits) {
+		this.serviceBoardHits = serviceBoardHits;
+	}
+	public String getNation() {
+		return nation;
+	}
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+	public String getMemberProfile() {
+		return memberProfile;
+	}
+	public void setMemberProfile(String memberProfile) {
+		this.memberProfile = memberProfile;
+	}
+	public String getServiceBoardImg() {
+		return serviceBoardImg;
+	}
+	public void setServiceBoardImg(String serviceBoardImg) {
+		this.serviceBoardImg = serviceBoardImg;
+	}
+	@Override
+	public String toString() {
+		return "ServiceBoardVO [serviceBoardNo=" + serviceBoardNo + ", serviceBoardTitle=" + serviceBoardTitle
+				+ ", serviceBoardContent=" + serviceBoardContent + ", serviceBoardCreateDate=" + serviceBoardCreateDate
+				+ ", serviceBoardHits=" + serviceBoardHits + ", nation=" + nation + ", memberProfile=" + memberProfile
+				+ ", serviceBoardImg=" + serviceBoardImg + "]";
+	}
+	
 }
